40727ef6eb24c31972a1f050294c6e7f07d80f45
ArtGeas <Jellu9@mail.ru>
2023-06-02 16:57:47 +0300

    djoser

--- skymarket/skymarket/settings.py
+++ skymarket/skymarket/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "djoser",
     "users",
     "ads",
     "redoc",
@@ -88,6 +89,10 @@ REST_FRAMEWORK = {
 }
 # TODO здесь мы настраиваем Djoser
 DJOSER = {
+    'SERIALIZERS': {
+        'user_create': 'users.serializers.UserRegistrationSerializer'
+    },
+    'LOGIN_FIELD': 'email'
 }
 
 # Database
--- skymarket/users/managers.py
+++ skymarket/users/managers.py
@@ -4,4 +4,31 @@ from django.contrib.auth.models import (
 # TODO здесь должен быть менеджер для модели Юзера.
 # TODO Поищите эту информацию в рекомендациях к проекту
 class UserManager(BaseUserManager):
-    pass
+
+    def create_user(self, email, first_name, last_name, phone, password=None):
+        if not email:
+            raise ValueError("Users must have email")
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            phone=phone,
+            role="user"
+        )
+        user.is_active = True
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, first_name, last_name, phone, password=None):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            phone=phone,
+            role="admin"
+        )
+        user.save(using=self._db)
+
+        return user
