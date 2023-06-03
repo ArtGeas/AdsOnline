cc32e336c7cd91743bb10e3be1be934eace33ba6
ArtGeas <Jellu9@mail.ru>
2023-06-03 21:24:58 +0300

    Ad model

--- skymarket/ads/models.py
+++ skymarket/ads/models.py
@@ -1,10 +1,19 @@
 from django.conf import settings
 from django.db import models
 
+from skymarket.users.models import User
+
 
 class Ad(models.Model):
     # TODO добавьте поля модели здесь
-    pass
+    title = models.CharField(max_length=200)
+    price = models.IntegerField()
+    description = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
 
 
 class Comment(models.Model):
