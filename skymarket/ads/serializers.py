7358b20aad54597d75be14ddc1a65edae5eea3ee
ArtGeas <Jellu9@mail.ru>
2023-06-06 23:42:55 +0300

    ads create

--- skymarket/ads/serializers.py
+++ skymarket/ads/serializers.py
@@ -1,8 +1,7 @@
 from rest_framework import serializers
 
-from ads.models import Ad
-
-from ads.models import Comment
+from ads.models import Ad, Comment
+from users.models import User
 
 
 # TODO Сериалайзеры. Предлагаем Вам такую структуру, однако вы вправе использовать свою
@@ -19,6 +18,13 @@ class AdSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ad
         fields = "__all__"
+
+    def create(self, validated_data):
+        author_id = validated_data.pop('author_id')
+        author = User.objects.get(id=author_id)
+
+        new_ad = Ad.objects.create(author_id=author, **validated_data)
+        return new_ad
 
 
 class AdDetailSerializer(serializers.ModelSerializer):
