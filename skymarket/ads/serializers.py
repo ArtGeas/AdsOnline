081c7ca61cbf9985d6b55cfeb9392bb3bcfca802
ArtGeas <Jellu9@mail.ru>
2023-06-07 21:35:54 +0300

    del def create

--- skymarket/ads/serializers.py
+++ skymarket/ads/serializers.py
@@ -19,13 +19,6 @@ class AdSerializer(serializers.ModelSerializer):
         model = Ad
         fields = "__all__"
 
-    def create(self, validated_data):
-        author_id = validated_data.pop('author_id')
-        # author = User.objects.get(id=author_id)
-
-        new_ad = Ad.objects.create(author_id=author_id, **validated_data)
-        return new_ad
-
 
 class AdDetailSerializer(serializers.ModelSerializer):
     # TODO сериалайзер для модели
